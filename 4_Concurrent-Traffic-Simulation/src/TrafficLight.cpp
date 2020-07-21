e98ea21ec3d075888600af872c703dd4f9310413
ikarHxC <linxsaturnos@gmail.com>
2020-07-21 03:39:23 +0200

    project 4 working

--- 4_Concurrent-Traffic-Simulation/src/TrafficLight.cpp
+++ 4_Concurrent-Traffic-Simulation/src/TrafficLight.cpp
@@ -2,6 +2,7 @@
 #include <random>
 #include "TrafficLight.h"
 #include <random>
+#include <future>
 
 /* Implementation of class "MessageQueue" */
 
@@ -13,7 +14,7 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
     std::unique_lock<std::mutex> uLock(_mtx);
-    _cond.wait(uLock);
+    _cond.wait(uLock, [this]{return !_queue.empty();});
     T lastElement = std::move(_queue.back());
     _queue.pop_back();
     return lastElement;
@@ -72,21 +73,30 @@ void TrafficLight::cycleThroughPhases()
     std::random_device rd;
     std::mt19937 generator(rd());
     std::uniform_int_distribution<int> distribution(4,6);
+    auto start = std::chrono::system_clock::now();
+    int random_time = distribution(generator);
 
-    auto start = std::chrono::steady_clock::now();
     while(true)
     {   
-        int random_time = distribution(generator);
         std::this_thread::sleep_for(std::chrono::milliseconds(1));
-        auto end = std::chrono::steady_clock::now();
+        auto end = std::chrono::system_clock::now();
         auto elapsed_time = std::chrono::duration_cast<std::chrono::seconds>(end - start).count();
-        if (elapsed_time > random_time) break;
+        if (elapsed_time > random_time){
+
+            _currentPhase = _currentPhase == red ? green : red;
+            //TODO: add send to msg queue
+            auto ftr = std::async(std::launch::async, 
+                                    &MessageQueue<TrafficLightPhase>::send,
+                                    &_msgQueue,
+                                    std::move(_currentPhase));
+            ftr.wait();
+            start = std::chrono::system_clock::now();
+            int random_time = distribution(generator);
+        }
+
 
     }
 
-    _currentPhase == red ? red : green;
-    //TODO: add send to msg queue
-    
     
 }
 
